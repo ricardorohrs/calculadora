5ca95a613b67bfd143e62792e016e32c0a6d1623
ricardoerohrs <rerohrs@inf.ufsm.br>
2024-02-01 18:07:04 -0300

    feat: Ajustes na estilização e na organização dos arquivos

---
+++ README.md
@@ -0,0 +1,26 @@
+# Calculadora
+### [O projeto pode ser visualizado clicando aqui.](https://ricardorohrs.github.io/calculadora/)
+## Objetivos
+O principal objetivo deste projeto é fornecer um desafio para testar as habilidades em desenvolvimento front-end.
++ Para a criação de uma calculadora simples e uma calculadora de juros
+  compostos, as tecnologias utilizadas foram apenas **HTML, CSS** (com auxílio do [Tailwind](https://tailwindcss.com/)) e **JavaScript**.
+## Principais Requisitos
+1. A calculadora deve permitir que os usuários insiram números.
+2. A calculadora deve ter botões para operações aritméticas básicas - adição,
+   subtração, multiplicação, divisão.
+3. A calculadora deve exibir o resultado do cálculo na tela.
+   Calculadora 3
+4. A calculadora deve ter uma opção para limpar o resultado do cálculo.
+5. A calculadora deve ter um botão para alternar para a calculadora de juros
+   compostos.
+6. A calculadora de juros compostos deve permitir que os usuários insiram o valor
+   principal (P), a taxa de juros (r) e o número de períodos de tempo (n).
+7. A calculadora de juros compostos deve exibir o valor futuro (A) do cálculo na
+   tela.
+8. A calculadora de juros compostos deve ter uma opção para limpar os valores
+   inseridos.
+9. A calculadora de juros compostos deve ter um botão para alternar de volta para
+   a calculadora simples.
+10. A calculadora deve apresentar o histórico como uma lista de cálculos anteriores
+    e seus resultados.
+11. A calculadora deve ter uma opção para limpar o histórico de cálculos.
---
+++ script.js
@@ -0,0 +1,71 @@
+function multiplyNode(node, count) {
+    for (let i = 0; i < count - 1; i++) {
+        const copy = node.cloneNode(true);
+        node.parentNode.insertBefore(copy, node);
+    }
+}
+
+multiplyNode(document.querySelector('.box'), window.innerHeight);
+
+if (window.innerWidth < 750) {
+    for (const item of document.getElementsByClassName("mobile-disabled")) {
+        item.style.color = '#232323';
+        item.style.backgroundColor = '#232323';
+    }
+}
+
+let showInterestCard = true;
+let showHistoryCard = true;
+let isOpen = false;
+
+const showInterest = () => {
+    showInterestCard = !showInterestCard;
+    document.getElementById("interest").style.left = showInterestCard ? '42%' : '23%';
+}
+
+const showHistory = () => {
+    showHistoryCard = !showHistoryCard;
+    document.getElementById("history-card").style.left = showHistoryCard ? '42%' : '65%';
+}
+
+const clearAll = () => {
+    return document.getElementById('result').value = "";
+}
+
+const display = (value, parentesis = false) => {
+    if (parentesis) {
+        isOpen = !isOpen;
+    }
+
+    return document.getElementById('result').value += value;
+}
+
+const calculateInterest = () => {
+    const c = document.getElementById('c').value;
+    const i = document.getElementById('i').value / 100;
+    const t = document.getElementById('t').value;
+
+    const result = c * Math.pow((1 + i), t);
+    document.getElementById('interest-result').value = result.toFixed(2);
+}
+
+const calculate = () => {
+    const history = document.getElementById('history-inner');
+    const expression = document.getElementById('result').value;
+
+    let result = document.getElementById('result').value = eval(document.getElementById('result').value);
+
+    let item = document.createElement('p');
+    let text = document.createTextNode(`${expression} = ${result}`);
+
+    item.appendChild(text);
+    history.appendChild(item);
+}
+
+document.addEventListener("keypress", function(event) {
+    if (event.key === 'Enter') {
+        calculate();
+    } else if(event.DOM_KEY_LOCATION_NUMPAD) {
+        document.getElementById('result').focus();
+    }
+});
